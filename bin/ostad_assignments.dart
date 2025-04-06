269254a292d575113d00e4a1b41aa759875a18d3
Fahim <fahimsafaat67@gmail.com>
2025-04-06 12:12:33 +0600

    Initial commit

---
+++ bin/ostad_assignments.dart
@@ -0,0 +1,5 @@
+import 'package:ostad_assignments/ostad_assignments.dart' as ostad_assignments;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ostad_assignments.calculate()}!');
+}
---
+++ test/ostad_assignments_test.dart
@@ -0,0 +1,8 @@
+import 'package:ostad_assignments/ostad_assignments.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
